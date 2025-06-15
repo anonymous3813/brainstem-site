b9effd1e7701fa8a3d369b32eb142a93760cd218
anonymous3813 <luojeff8@gmail.com>
2025-06-15 16:50:21 -0700

    more stuff

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,13 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      entries: ['*']
-    }
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({ fallback: 'index.html' }),
+		prerender: {
+			entries: ['*']
+		}
+	}
 };
 
 export default config;
